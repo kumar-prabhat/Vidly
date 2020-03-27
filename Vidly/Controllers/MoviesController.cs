fb32b89292c3a1012987dc6edb9ac3b3d58a621a
Prabhat Kumar <prabhatkumar@rarecrew.com>
2020-03-27 13:36:27 +0530

    Initial commit

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,21 +10,20 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
-        // GET: Movies
+        // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Harry Potter!"};
-            var customers = new List<Customer>()
+            var movie = new List<Movie>()
             {
-                new Customer{ Name = "Customer 1"},
-                new Customer{Name = "Customer 2"}
+                new Movie{Id = 1, Name = "Harry Potter!"},
+                new Movie{Id = 2, Name = "Avengers!"},
+                new Movie{Id = 2, Name = "Iron Man!"}
             };
-            var vm = new RandomMovieViewModel
+          var vm = new RandomMovieViewModel
             {
                 Movie = movie,
-                Customers = customers
             };
-            return View(vm);
+            return View("Movies", vm);
 
 
            // return Content("Hello World"); //Return the text Hello World
@@ -33,10 +32,35 @@ namespace Vidly.Controllers
            // return RedirectToAction("Index","Home",new{page=1,sortBy="name"});
         }
 
-        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        //public ActionResult ByReleaseDate(int year,int month)
-        //{
-        //    return Content(year+"/"+month);
-        //}
+        public ActionResult Details(int id)
+        {
+            var name = "";
+            switch (id)
+            {
+                case 1:
+                    name = "Harry Potter!";
+                    break;
+                case 2:
+                    name = "Avengers";
+                    break;
+                case 3:
+                    name = "IronMan!";
+                    break;
+                default:
+                    return HttpNotFound();
+            }
+            var movie = new Movie()
+            {
+                Name = name
+            };
+           
+            return PartialView("_Details", movie);
+        }
+
+        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
     }
 }
